c8a0a1363b2ee371b6a9e381578d9a77c5cb4dbf
LeonardoGraceGeller <3428660343@qq.com>
2024-05-16 01:15:27 +0800

    ONIX

--- onix/src/kernel/io.asm
+++ onix/src/kernel/io.asm
@@ -10,7 +10,58 @@ inb:
 
     xor eax,eax
     mov edx,[ebp+8];port
-    in al,dx
+    in al,dx;将端口号dx的8 bit 输入到al
+
+    jmp $+2;延迟时间
+    jmp $+2
+    jmp $+2
+    
+    leave
+    ret
+
+global inw;将inw导出
+
+inw:
+    push ebp
+    mov ebp,esp;保存帧
+
+    xor eax,eax
+    mov edx,[ebp+8];port
+    in ax,dx;将端口号dx的8 bit 输入到ax
+
+    jmp $+2;延迟时间
+    jmp $+2
+    jmp $+2
+    
+    leave
+    ret
+
+global outb;将outb导出
+
+outb:
+    push ebp
+    mov ebp,esp;保存帧
+
+    mov edx,[ebp+8];port
+    mov eax,[ebp+12];value
+    out dx,ax;将al中的8bit输入出道 端口号dx
+
+    jmp $+2;延迟时间
+    jmp $+2
+    jmp $+2
+    
+    leave
+    ret
+
+global outw;将outw导出
+
+outw:
+    push ebp
+    mov ebp,esp;保存帧
+
+    mov edx,[ebp+8];port
+    mov eax,[ebp+12];value
+    out dx,ax;将ax中的8bit输入出到 端口号dx
 
     jmp $+2;延迟时间
     jmp $+2
--- onix/src/kernel/main.c
+++ onix/src/kernel/main.c
@@ -2,8 +2,13 @@
 #include <onix/io.h>
 #include <onix/types.h>
 
-#define CRT_ADDR_REG 0x3d5
-#define CRT_DATA_REG 0x3d6
+//-CRT 地址寄存器 0x3D4
+//-CRT 数据寄存器 0x3D5
+//-CRT 光标位置 -高位 0xe
+//-CRT 光标位置 -低位 0xf
+
+#define CRT_ADDR_REG 0x3d4
+#define CRT_DATA_REG 0x3d5
 
 #define CRT_CURSOR_H 0xe
 #define CRT_CURSOR_L 0xf
@@ -11,5 +16,13 @@
 
 void kernel_init()
 {
-   u8 data = inb(CRT_DATA_REG);
+   outb(CRT_ADDR_REG,CRT_CURSOR_H);
+   u16 pos = inb(CRT_DATA_REG)<<8;
+   outb(CRT_ADDR_REG,CRT_CURSOR_L);
+   pos|=inb(CRT_DATA_REG);
+
+   outb(CRT_ADDR_REG,CRT_CURSOR_H);
+   outb(CRT_DATA_REG,0);
+   outb(CRT_ADDR_REG,CRT_CURSOR_L);
+   outb(CRT_DATA_REG,123);
 }
