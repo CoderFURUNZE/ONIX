0af183e7691ea86586160b070378e228a4c65581
LeonardoGraceGeller <3428660343@qq.com>
2024-07-31 15:06:44 +0800

    ONIX

--- onix/src/include/onix/syscall.h
+++ onix/src/include/onix/syscall.h
@@ -9,4 +9,7 @@ typedef enum syscall_t
     SYS_NR_YIELD,
 } syscall_t;
 
+u32 test();
+void yield();
+
 #endif
--- onix/src/include/onix/task.h
+++ onix/src/include/onix/task.h
@@ -47,4 +47,6 @@ typedef struct task_frame_t
 task_t *running_task();
 void schedule();
 
+void task_yield();
+
 #endif
